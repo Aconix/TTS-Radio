0696b4294bb9021c0eea6b78a131a3e03a8579db
Aconix <73419062+Aconix@users.noreply.github.com>
2020-11-17 16:50:34 +1100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,32 @@
+# TTS Radio
+# Copyright (c) 2020 Aconix
+
+
+
+# This is what is said before the announcement.
+upNext1 = ("Up next is ")
+#           songname
+upNext2 = (".")
+
+
+
+# This is what is said before the next song is played.
+announcement = ("You're listening to Text to Speech Radio.")
+
+
+
+
+
+# This is what is said after a song has been played.
+justPlayed1 = ("That was ")
+#               songname
+justPlayed2 = (".")
+
+
+
+
+
+
+# This is what the idle message is in the console.
+# I don't see a point to changing it, but if you want to, here you go.
+idleMessage = ("TTS Radio - The easy way to make your own radio station.")
---
+++ radio.py
@@ -0,0 +1,46 @@
+# TTS Radio
+# Copyright (c) 2020 Aconix
+
+import config
+import win32com.client as wincl
+import winsound
+import time
+import os
+import random
+
+speak = wincl.Dispatch("SAPI.SpVoice")
+
+files = os.listdir("sounds")
+file_count = len(files)
+
+print(r"""
+ /$$$$$$$$ /$$$$$$$$ /$$$$$$        /$$$$$$$                  /$$ /$$          
+|__  $$__/|__  $$__//$$__  $$      | $$__  $$                | $$|__/          
+   | $$      | $$  | $$  \__/      | $$  \ $$  /$$$$$$   /$$$$$$$ /$$  /$$$$$$ 
+   | $$      | $$  |  $$$$$$       | $$$$$$$/ |____  $$ /$$__  $$| $$ /$$__  $$
+   | $$      | $$   \____  $$      | $$__  $$  /$$$$$$$| $$  | $$| $$| $$  \ $$
+   | $$      | $$   /$$  \ $$      | $$  \ $$ /$$__  $$| $$  | $$| $$| $$  | $$
+   | $$      | $$  |  $$$$$$/      | $$  | $$|  $$$$$$$|  $$$$$$$| $$|  $$$$$$/
+   |__/      |__/   \______/       |__/  |__/ \_______/ \_______/|__/ \______/ 
+
+Copyright (c) 2020 Aconix
+    """)
+
+
+time.sleep(2)
+print("REFER TO THE README FOR HELP")
+print()
+time.sleep(2)
+print("The sounds folder contains " + str(file_count) + " song(s)")
+
+i = 0
+
+while i <= 10:
+    music = random.choice(files)
+    musicNoExt = os.path.splitext(music)[0]
+    speak.Speak(config.upNext1 + musicNoExt + config.upNext2)
+    speak.Speak(config.announcement)
+    print ("Now Playing: " + musicNoExt)
+    winsound.PlaySound("sounds/" + music, winsound.SND_ALIAS)
+    print(config.idleMessage)
+    speak.Speak(config.justPlayed1 + musicNoExt + config.justPlayed2)
